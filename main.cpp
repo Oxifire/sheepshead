ab93c8d9e5222c5a7095717b87f29a2ad830c611
adam subat <subat002@umn.edu>
2015-04-14 14:56:51 -0500

    Beginning main.cpp.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*	Welcome to the MAIN program.
+ * 	Author: Adam Subat
+ *	Initializes SASE, allows you to run the following benchmarks:
+ *		1 All-RAND
+ *		2 All-BTT
+ *		3 All-CDT
+ *		4 BTT-vs-RAND
+ *		5 CDT-vs-RAND
+ *		6 CDT-vs-BTT
+ *	Each benchmark showcases an agent's ability to play consistently and
+ *	outcompete it's predeccsessors.
+ * 
+ *	RAND:	Makes completely random decisions.
+ * 	BTT:	Makes decisions based solely on trick-taking ability.
+ * 	CDT:	Makes decisions on a set of known evidence, as discussed in
+ * 			the paper.
+ */
+
+#include <stdio.h>
+#include <iostream>
+
+int main{
+	cout << "What would you like to do?";
+}
